7c45a75aff6a95f161a9d4b4ea9cbcbb56f45907
msimgit <mario.simarro@coiim.es>
2026-07-18 18:21:10 +0200

    INcluye el ejercicio del SP15 Fundamentos Redux Toolkit

---
+++ Module 3. Frontend/SP15_Fundamentos_Redux_Toolkit/src/App.jsx
@@ -0,0 +1,14 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./features/counter/counterSlice";
+
+export default function App() {
+  const count = useSelector(state => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1>Contador: {count}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  );
+}
